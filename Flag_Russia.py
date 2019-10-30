2e61fed808137b636525221694266cb49367165f
Екатерина Лысенок <developer@yandex.ru>
2019-10-30 19:08:50 +0300

    Нарисован красный флаг

--- Flag_Russia.py
+++ Flag_Russia.py
@@ -5,12 +5,17 @@ def draw_flagpole():
     color = pygame.Color("bisque4")
     pygame.draw.rect(screen, color, (20, 20, 10, 200), 0)
 
+def draw_red_flag():
+    color = pygame.Color("firebrick1")
+    pygame.draw.rect(screen, color, (26, 20, 130, 35*3), 0)
+
 
 pygame.init()
 size = width, height = 200, 250
 screen = pygame.display.set_mode(size)
 
 draw_flagpole()
+draw_red_flag()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
